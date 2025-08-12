2f2a3b49d5108bacfbf3d9073b6b25d108bf70d9
leomet07 <47071760+leomet07@users.noreply.github.com>
2025-08-12 15:18:43 -0400

    histogram plots for platform heat index  for different number of services

--- analyze_variable_impact.py
+++ analyze_variable_impact.py
@@ -2,6 +2,8 @@ import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
 from pprint import pprint
+import seaborn as sns
+
 
 plt.rcParams.update(
     {
@@ -16,7 +18,7 @@ plt.rcParams.update(
 cuny_df = pd.read_csv("CUNY_MTA.csv")
 cuny_df["Timestamp"] = pd.to_datetime(cuny_df["Timestamp"], errors="coerce")
 
-cuny_df = cuny_df[cuny_df["Platform level heat index"] > 80]
+# cuny_df = cuny_df[cuny_df["Platform level heat index"] > 80]
 cuny_df = cuny_df[cuny_df["Timestamp"].dt.month == 7]  # only july
 
 cuny_df["Number of services"] = cuny_df.apply(
@@ -92,11 +94,49 @@ cuny_df.boxplot(
 )
 plt.xlabel("Number of Services")
 plt.ylabel("Platform Level Heat Index (°F)")
-plt.title("July 2024 and July 2025", fontweight="bold")
+plt.suptitle("July 2024 and July 2025", fontweight="bold")
+
+# Stacked histogram plots
+# fig, axs = plt.subplots(cuny_df["Number of services"].max())
+fig, ax = plt.subplots()  # 1 subplot
+fig.set_figwidth(14)
+fig.set_figheight(7)
+fig.suptitle("July 2024 and July 2025", fontweight="bold")
+
+for number_of_services in range(1, cuny_df["Number of services"].max() + 1):
+    cuny_df_only_this_number_of_services = cuny_df[
+        cuny_df["Number of services"] == number_of_services
+    ]
+
+    sns.kdeplot(
+        cuny_df_only_this_number_of_services["Platform level heat index"],
+        ax=ax,
+        label=f"{number_of_services} service{"" if number_of_services == 1 else "s"}",
+        linewidth=2,
+    )
+    # Median heat index
+    median_heat_index = np.median(
+        cuny_df_only_this_number_of_services["Platform level heat index"]
+    )
+    plt.axvline(
+        x=median_heat_index,
+        color=ax.get_lines()[-1].get_color(),
+        linestyle="--",
+        linewidth=1.5,
+    )
+
+    ax.set_xlabel("Platform Level Heat Index")
+    ax.set_ylabel("Density")
+ax.legend()
+ax.set_xlim(75, 125)
+fig.tight_layout()
+plt.savefig(f"test.jpg", bbox_inches="tight", dpi=400)
 
 # Scatter plot for TPH
 plt.figure("TPH vs Platform Level Heat Index")
 plt.scatter(cuny_df["Total TPH"], cuny_df["Platform level heat index"])
 plt.xlabel("Total TPH")
 plt.ylabel("Platform Level Heat Index")
+
+# Final show
 plt.show()
