3504e7342fe358b31b5959c0c1a4e8e761049109
leomet07 <47071760+leomet07@users.noreply.github.com>
2025-08-13 09:35:21 -0400

    Add scatterplot for citywide highs vs station; boxplot tweaks; F to C

--- analyze_variable_impact.py
+++ analyze_variable_impact.py
@@ -21,6 +21,11 @@ CHART_OUT_DIR = "paper_charts"
 if not os.path.exists(CHART_OUT_DIR):
     os.makedirs(CHART_OUT_DIR)
 
+
+def f_to_c(f):
+    return (f - 32) * (5 / 9)
+
+
 cuny_df = pd.read_csv("CUNY_MTA.csv")
 cuny_df["Timestamp"] = pd.to_datetime(cuny_df["Timestamp"], errors="coerce")
 
@@ -86,21 +91,42 @@ cuny_df["Total TPH"] = cuny_df.apply(
     calc_tph,
     axis=1,
 )
+cuny_df["Platform level heat index (°C)"] = cuny_df.apply(
+    lambda row: f_to_c(row["Platform level heat index"]),
+    axis=1,
+)
 
-print(cuny_df)
+# -------------------------------------------------------------------------------------------------
 
-# Plots for Number of services
-cuny_df.boxplot(
-    column="Platform level heat index",
+# Box Plots for Number of services
+axes = cuny_df.boxplot(
+    column="Platform level heat index (°C)",
     by="Number of services",
     grid=False,
     vert=True,
     patch_artist=True,
-    return_type="dict",
+    figsize=(14, 7),
+    return_type="axes",
+    medianprops=dict(color="red", linewidth=2),
+    whiskerprops=dict(color="black", linewidth=3),
+    capprops=dict(color="black", linewidth=3),
+    showmeans=False,
 )
+
+axes[0].set_title("")
+plt.ylim(20, 50)
 plt.xlabel("Number of Services")
-plt.ylabel("Platform Level Heat Index (°F)")
-plt.suptitle("July 2024 and July 2025", fontweight="bold")
+plt.ylabel("Platform Level Heat Index (°C)")
+plt.suptitle("July 2024 and July 2025 Platform Level Heat Index", fontweight="bold")
+plt.tight_layout()
+
+plt.savefig(
+    os.path.join(CHART_OUT_DIR, "box_plot_platform_heat_index_by_service.jpg"),
+    bbox_inches="tight",
+    dpi=400,
+)
+
+# -------------------------------------------------------------------------------------------------
 
 # Stacked histogram plots
 # fig, axs = plt.subplots(cuny_df["Number of services"].max())
@@ -115,14 +141,14 @@ for number_of_services in range(1, cuny_df["Number of services"].max() + 1):
     ]
 
     sns.kdeplot(
-        cuny_df_only_this_number_of_services["Platform level heat index"],
+        cuny_df_only_this_number_of_services["Platform level heat index (°C)"],
         ax=ax,
         label=f"{number_of_services} service{"" if number_of_services == 1 else "s"}",
         linewidth=2,
     )
     # Median heat index
     median_heat_index = np.median(
-        cuny_df_only_this_number_of_services["Platform level heat index"]
+        cuny_df_only_this_number_of_services["Platform level heat index (°C)"]
     )
     plt.axvline(
         x=median_heat_index,
@@ -131,10 +157,10 @@ for number_of_services in range(1, cuny_df["Number of services"].max() + 1):
         linewidth=1.5,
     )
 
-    ax.set_xlabel("Platform Level Heat Index")
-    ax.set_ylabel("Density")
+ax.set_xlabel("Platform Level Heat Index (°C)")
+ax.set_ylabel("Density")
 ax.legend()
-ax.set_xlim(75, 125)
+ax.set_xlim(22, 48)
 fig.tight_layout()
 plt.savefig(
     os.path.join(CHART_OUT_DIR, "histogram_platform_heat_index_by_service.jpg"),
@@ -142,11 +168,68 @@ plt.savefig(
     dpi=400,
 )
 
+# -------------------------------------------------------------------------------------------------
+
 # Scatter plot for TPH
 plt.figure("TPH vs Platform Level Heat Index")
-plt.scatter(cuny_df["Total TPH"], cuny_df["Platform level heat index"])
+plt.scatter(cuny_df["Total TPH"], cuny_df["Platform level heat index (°C)"])
 plt.xlabel("Total TPH")
-plt.ylabel("Platform Level Heat Index")
+plt.ylabel("Platform Level Heat Index (°C)")
+
+# -------------------------------------------------------------------------------------------------
+
+
+# Plot for citywide temps
+plt.figure("Citywide Temperature vs Platform Heat Index", (14, 7))
+START_DATE = "2025-07-01"
+END_DATE = "2025-07-12"
+
+
+citywide_df = pd.read_csv("citywide.csv")
+citywide_df["Date"] = pd.to_datetime(citywide_df["Date"])
+citywide_df_in_date_range = citywide_df[
+    (citywide_df["Date"] >= START_DATE) & (citywide_df["Date"] <= END_DATE)
+]
+plt.scatter(
+    citywide_df_in_date_range["Date"],
+    f_to_c(citywide_df_in_date_range["High Temp (°F)"]),
+    label="Citywide High Temperature",
+)
+
+filtered_cuny_df = cuny_df[
+    (cuny_df["Timestamp"] >= START_DATE) & (cuny_df["Timestamp"] <= END_DATE)
+]
+
+
+# Take mean of each days
+xs = []
+ys = []
+
+for day in list(pd.date_range(start=START_DATE, end=END_DATE, freq=f"1D")):
+    this_day_cuny_df = cuny_df[cuny_df["Timestamp"].dt.date == day.date()]
+    this_day_cuny_df = this_day_cuny_df[
+        (this_day_cuny_df["Timestamp"].dt.hour >= 12)
+        & (this_day_cuny_df["Timestamp"].dt.hour < 14)
+    ]
+    median_var = this_day_cuny_df["Platform level air temperature"].median()
+    xs.append(day.date())
+    ys.append(f_to_c(median_var))
+
+plt.scatter(
+    xs,
+    ys,
+    label="Median Platform Level Air Temperature (°C) [12:00 to 14:00]",
+)
+plt.xlabel("Date")
+plt.ylabel("Air Temperature (°C)")
+plt.legend()
+plt.savefig(
+    os.path.join(CHART_OUT_DIR, "time_series_citywide_to_median_air_temp.jpg"),
+    bbox_inches="tight",
+    dpi=400,
+)
+# -------------------------------------------------------------------------------------------------
+
 
 # Final show
 plt.show()
